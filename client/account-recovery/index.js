9530cffa963833ad45f83510f8536859725f2e2a
James Tien <southp0105@gmail.com>
2017-02-16 11:03:54 +0800

    Rename reset-password-email-sent to reset-password-email-form

--- client/account-recovery/index.js
+++ client/account-recovery/index.js
@@ -5,6 +5,7 @@ import {
 	lostPassword,
 	forgotUsername,
 	resetPassword,
+	resetPasswordEmailForm,
 	resetPasswordByTransactionId,
 	redirectLoggedIn
 } from './controller';
@@ -14,5 +15,6 @@ export default function( router ) {
 	router( '/account-recovery', redirectLoggedIn, lostPassword );
 	router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
 	router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
+	router( '/account-recovery/reset-password/email-form', redirectLoggedIn, resetPasswordEmailForm );
 	router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
 }
