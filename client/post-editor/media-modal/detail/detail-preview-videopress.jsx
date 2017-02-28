16333f045805ff0608e89ed2465843d6e6b37405
Donna Peplinskie <donnapep@gmail.com>
2017-02-28 14:58:22 -0500

    Handle error if VideoPress script failed to load

--- client/post-editor/media-modal/detail/detail-preview-videopress.jsx
+++ client/post-editor/media-modal/detail/detail-preview-videopress.jsx
@@ -2,12 +2,16 @@
  * External dependencies
  */
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import debug from 'debug';
 
 /**
  * Internal dependencies
  */
 import { loadScript, removeScriptCallback } from 'lib/load-script';
+import {
+	setVideoEditorHasScriptLoadError,
+} from 'state/ui/editor/video-editor/actions';
 
 /**
  * Module variables
@@ -64,7 +68,15 @@ class EditorMediaModalDetailPreviewVideoPress extends Component {
 		const {
 			isPlaying,
 			item,
+			setHasScriptLoadError,
 		} = this.props;
+
+		if ( error ) {
+			log( `Script${ error.src } failed to load.` );
+			setHasScriptLoadError();
+
+			return;
+		}
 
 		if ( typeof window !== 'undefined' && window.videopress ) {
 			this.player = window.videopress( item.videopress_guid, this.video, {
@@ -98,4 +110,9 @@ class EditorMediaModalDetailPreviewVideoPress extends Component {
 	}
 }
 
-export default EditorMediaModalDetailPreviewVideoPress;
+export default connect(
+	null,
+	{
+		setHasScriptLoadError: setVideoEditorHasScriptLoadError,
+	}
+)( EditorMediaModalDetailPreviewVideoPress );
